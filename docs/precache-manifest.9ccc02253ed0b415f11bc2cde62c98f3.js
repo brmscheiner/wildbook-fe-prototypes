6e01a59ed389a98f28d15076978463a7559eba4b
Ben Scheiner <brmscheiner@gmail.com>
2020-02-12 08:37:52 -0800

    Eject, build to docs folder for github pages integration

---
+++ docs/precache-manifest.9ccc02253ed0b415f11bc2cde62c98f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5555a47905d463a8cd23eb982ac910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fe3bdd7a03b7e10bc42",
+    "url": "/static/js/2.74964094.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/static/js/2.74964094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb118aced35ff592921e",
+    "url": "/static/js/main.0ab73f9e.chunk.js"
+  },
+  {
+    "revision": "8a21a9610ca2905bab57",
+    "url": "/static/js/runtime-main.89f24a37.js"
+  },
+  {
+    "revision": "f3a53693fdbaf052e8f883c8fa2efcd6",
+    "url": "/static/media/blog-post.1.f3a53693.md"
+  },
+  {
+    "revision": "99cbba3ba72c097ec32daa6f5eed33ec",
+    "url": "/static/media/blog-post.2.99cbba3b.md"
+  },
+  {
+    "revision": "8d6e276be069ee011ba792969f01d327",
+    "url": "/static/media/blog-post.3.8d6e276b.md"
+  }
+]);
