23fe6984457be532ec0f4cdabc46ac28eb3a713e
Ben Scheiner <brmscheiner@gmail.com>
2020-02-12 08:50:59 -0800

    Set homepage

---
+++ dist/precache-manifest.d4dd5216488cde1de167c2326dc14dc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f3b341170ff9a216c73af95080efba",
+    "url": "/wildbook-fe-prototypes/index.html"
+  },
+  {
+    "revision": "5fe3bdd7a03b7e10bc42",
+    "url": "/wildbook-fe-prototypes/static/js/2.74964094.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/wildbook-fe-prototypes/static/js/2.74964094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb118aced35ff592921e",
+    "url": "/wildbook-fe-prototypes/static/js/main.0ab73f9e.chunk.js"
+  },
+  {
+    "revision": "86d7c2b67b510718f51a",
+    "url": "/wildbook-fe-prototypes/static/js/runtime-main.7aa50da3.js"
+  },
+  {
+    "revision": "f3a53693fdbaf052e8f883c8fa2efcd6",
+    "url": "/wildbook-fe-prototypes/static/media/blog-post.1.f3a53693.md"
+  },
+  {
+    "revision": "99cbba3ba72c097ec32daa6f5eed33ec",
+    "url": "/wildbook-fe-prototypes/static/media/blog-post.2.99cbba3b.md"
+  },
+  {
+    "revision": "8d6e276be069ee011ba792969f01d327",
+    "url": "/wildbook-fe-prototypes/static/media/blog-post.3.8d6e276b.md"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ccc02253ed0b415f11bc2cde62c98f3.js"
+  "/wildbook-fe-prototypes/precache-manifest.d4dd5216488cde1de167c2326dc14dc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wildbook-fe-prototypes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
