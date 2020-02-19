7d58fa6f5ca86e2ef62f6dffe1bbe82b0d173398
Ben Scheiner <brmscheiner@gmail.com>
2020-02-19 13:28:08 -0500

    Updates

---
+++ precache-manifest.6923e9cacc5d42d665e68d7d059a3ba7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a82dc3572e117b9316f2f8a4d18ca34",
+    "url": "/wildbook-fe-prototypes/index.html"
+  },
+  {
+    "revision": "e8e79b824b080ef95a5e",
+    "url": "/wildbook-fe-prototypes/static/js/2.3d940567.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/wildbook-fe-prototypes/static/js/2.3d940567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81d2770f4d0a94a00cf4",
+    "url": "/wildbook-fe-prototypes/static/js/main.dafeb8aa.chunk.js"
+  },
+  {
+    "revision": "86d7c2b67b510718f51a",
+    "url": "/wildbook-fe-prototypes/static/js/runtime-main.7aa50da3.js"
+  },
+  {
+    "revision": "ffdb1096ad13084831712c6de10c7d0e",
+    "url": "/wildbook-fe-prototypes/static/media/jon.ffdb1096.jpeg"
+  },
+  {
+    "revision": "b658bdc622336a7b663b385e3f4c16e9",
+    "url": "/wildbook-fe-prototypes/static/media/jon2.b658bdc6.jpeg"
+  },
+  {
+    "revision": "a36bc90c1405b0d1e625446d3e1e5a3f",
+    "url": "/wildbook-fe-prototypes/static/media/sahara-conservation.a36bc90c.png"
+  },
+  {
+    "revision": "e370ca584471c8d9165ad4d05e2b201e",
+    "url": "/wildbook-fe-prototypes/static/media/shark1.e370ca58.png"
+  },
+  {
+    "revision": "fc277223171e85472c5e62e32d5e06b1",
+    "url": "/wildbook-fe-prototypes/static/media/shark2.fc277223.jpeg"
+  }
+]);
