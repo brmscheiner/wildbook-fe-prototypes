de82694cf359fef8717b46383b14dc1aecdfd856
Ben Scheiner <brmscheiner@gmail.com>
2020-03-11 14:07:38 -0500

    Add back button

--- src/pages/StandardSubmit.js
+++ src/pages/StandardSubmit.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
@@ -10,6 +10,7 @@ import UploadImages from "../components/UploadImages";
 import ProcessingImages from "../components/ProcessingImages";
 import GroupImages from "../components/GroupImages";
 import AddMetadata from "../components/AddMetadata";
+import BackIcon from "@material-ui/icons/ChevronLeft";
 
 export default function StandardSubmit() {
   const [step, setStep] = useState(0);
@@ -19,7 +20,7 @@ export default function StandardSubmit() {
   return (
     <div
       style={{
-        margin: '80px auto',
+        margin: "80px auto",
         width: "100%",
         maxWidth: 800,
         display: "flex",
@@ -42,6 +43,20 @@ export default function StandardSubmit() {
           <StepLabel>Add metadata</StepLabel>
         </Step>
       </Stepper>
+      <div style={{ width: "100%", textAlign: "left", marginLeft: 180 }}>
+        <Button
+          onClick={() => {
+            if (step === 2) {
+              setStep(0)
+            } else {
+              setStep(Math.max(step - 1, 0));
+            }
+          }}
+          startIcon={<BackIcon />}
+        >
+          Back
+        </Button>
+      </div>
       {step === 0 && <UploadImages setStep={setStep} />}
       {step === 1 && <ProcessingImages step={step} setStep={setStep} />}
       {step === 2 && <GroupImages setStep={setStep} />}
